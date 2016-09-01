e31181146e5368b0ff7ae0a43e6628d647ed0ccb
antsmc2 <antsmc2@yahoo.com>
2016-09-01 18:44:27 +0100

    file share shows more user freiendly file size

--- static/js/meetings.js
+++ static/js/meetings.js
@@ -614,10 +614,20 @@ function handleReceivedBinaryMessage(msg) {
     downloadAnchor.href = URL.createObjectURL(received);
     downloadAnchor.download = msg['fileName'];
     downloadAnchor.textContent =
-      'Click to download \'' + msg['fileName'] + '\' (' + msg['fileSize'] + ' bytes)';
+      'Click to download \'' + msg['fileName'] + '\' (' + formatBytes(msg['fileSize']) + ')';
     downloadAnchor.style.display = 'block';
 
   }
+}
+
+//Apercu@http://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
+function formatBytes(bytes,decimals) {
+   if(bytes == 0) return '0 Byte';
+   var k = 1024;
+   var dm = decimals + 1 || 3;
+   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+   var i = Math.floor(Math.log(bytes) / Math.log(k));
+   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
 
 
